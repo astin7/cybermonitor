d2d4efb495c1e20f0f37bdd343161890eda275b3
astin7 <astinhuynh@gmail.com>
2026-01-19 11:38:22 -0500

    Test2

--- CyberMonitor_Web/src/main.jsx
+++ CyberMonitor_Web/src/main.jsx
@@ -7,4 +7,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+  <BrowserRouter basename="/cybermonitor-website">
+  <App />
+  </BrowserRouter>
 )
